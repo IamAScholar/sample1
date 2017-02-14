7f8aca3d216b690f0ff5b27bfa5709c121f8c112
vagrant <vagrant@homestead>
2017-02-14 09:40:55 +0000

    create user

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,5 @@
 <?php
-
-namespace App/Model;
-
+namespace App\Models;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
