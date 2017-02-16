041e7def3125811fa157d02f501b8e572dc1fe11
vagrant <vagrant@homestead>
2017-02-16 02:36:26 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,4 +34,8 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+    public function gravatar($size = 1000 ){
+      $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
